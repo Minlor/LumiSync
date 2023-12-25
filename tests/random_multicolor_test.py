0d1c2cb968db1967b279a878cec2187be75eea52
Minlor <40441731+Minlor@users.noreply.github.com>
2023-12-26 00:07:56 +0100

    Create random_multicolor_test.py

---
+++ tests/random_multicolor_test.py
@@ -0,0 +1,20 @@
+import random
+import time
+
+#Local
+from utils import SendData
+
+colors = []
+for i in range(5):
+    colors.append([random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)])
+
+print("Attempting to turn on razer mode...")
+SendData.send_razer_on_off(True)
+print("Attempting to send colors...")
+print(colors)
+SendData.send_razer_data(SendData.convert_colors(colors))
+print("Colors sent!")
+time.sleep(5)
+print("Attempting to turn off razer mode, it might take up to a minute for the lights to refresh...")
+SendData.send_razer_on_off()
+
