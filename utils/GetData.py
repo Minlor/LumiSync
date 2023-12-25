b1d0b8ff30923eeca6950b3170ae7538251bc1eb
Minlor <40441731+Minlor@users.noreply.github.com>
2023-12-25 23:34:56 +0100

    Create GetData.py

---
+++ utils/GetData.py
@@ -0,0 +1,18 @@
+import json
+import time
+
+from utils import GetDevices
+
+def get_device_data():
+    try:
+        with open("Settings.json", "r") as f:
+            data = json.load(f)
+
+        if time.time() - data.get("Time", 0) > 86400:
+            print("Device data is older than 24 hours, requesting new data...")
+            data = GetDevices.start()
+        return data
+    except:
+        print("Settings.json not found/corrupted, requesting new data...")
+        data = GetDevices.start()
+        return data
