f061c0cafca3eb63895fcabcd6bbb32181a24037
Patrice Brend'amour <patrice@brendamour.net>
2024-03-16 22:23:29 +0100

    Added support for multiple devices
    Now supports multiple devices in settings

--- utils/GetDevices.py
+++ utils/GetDevices.py
@@ -13,8 +13,9 @@ def start():
     print(f"{colorama.Fore.YELLOW}Trying to find device...")
     data = listen()
     print(f"{colorama.Fore.GREEN}Device found!")
-    new_data = writeJSON(json.loads(data[0]))
-    return(new_data)
+    settings = parseMessages(data)
+    new_data = writeJSON(settings)
+    return(settings)
 
 def requestScan():
     data = {"msg":{"cmd":"scan","data":{"account_topic":"reserve"}}}
@@ -24,24 +25,50 @@ def requestScan():
 
 def listen():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.settimeout(5)
+    sock.settimeout(10)
     sock.bind(('', listen_port))
+    messages = []
     try:
-        message, address = sock.recvfrom(1024)
+        while True:
+            message, address = sock.recvfrom(1024)
+            print(f"{colorama.Fore.LIGHTGREEN_EX}Received message from: ", address)
+            messages.append(message)
     except socket.timeout:
-        print(f"{colorama.Fore.RED}Error: No device found!")
-        exit(1)
-    print(f"{colorama.Fore.LIGHTGREEN_EX}Received message from: ", address)
-    return(message, address)
+        if len(messages) == 0:
+            print(f"{colorama.Fore.RED}Error: No device found!")
+            exit(1)
+    
+    return messages
 
-def writeJSON(data):
-    with open("settings.json", "w") as f:
-        data = {
-            "Model": data["msg"]["data"]["sku"],
-            "Device_IP": data["msg"]["data"]["ip"],
-            "Device_Port": 4003,
-            "Time": time.time()
+def parseMessages(messages):
+    try:
+        with open("Settings.json", "r") as f:
+                settings = json.load(f)
+    except FileNotFoundError:
+        settings = {
+            "time": time.time(),
+            "devices": [],
+            "selectedDevice": 0
         }
-        json.dump(data, f)
+    for deviceJson in messages:
+        device = json.loads(deviceJson)
+        existingDevice = next((x for x in settings["devices"] if x["MAC"] == device["msg"]["data"]["device"]), None)
+        if existingDevice is None:
+            data = {
+                "MAC": device["msg"]["data"]["device"],
+                "Model": device["msg"]["data"]["sku"],
+                "Device_IP": device["msg"]["data"]["ip"],
+                "Device_Port": 4003,
+                "Time": time.time()
+            }
+            settings["devices"].append(data)
+        else:
+            existingDevice["Device_IP"] = device["msg"]["data"]["ip"]
+            existingDevice["Time"] = time.time()
+    return settings
+
+def writeJSON(settings):
+    with open("settings.json", "w") as f:
+        json.dump(settings, f)
     print(f"{colorama.Fore.LIGHTGREEN_EX}Data written to Settings.json")
-    return(data)
+    return(settings)
