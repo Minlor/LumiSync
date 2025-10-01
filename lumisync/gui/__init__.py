6b607bab7ff145469203d7776a37cf3ee21a6d16
Minlor <40441731+Minlor@users.noreply.github.com>
2025-10-01 20:08:17 +0200

    Refactor imports and update __all__ in __init__.py

--- lumisync/gui/__init__.py
+++ lumisync/gui/__init__.py
@@ -3,8 +3,9 @@ Views package for the LumiSync GUI.
 This package contains views for displaying the application's user interface.
 """
 
-from .devices_tab import DevicesTab
-from .modes_tab import ModesTab
-from .color_tab import ColorTab
+from .app import run_gui
+from .views.devices_tab import DevicesTab
+from .views.modes_tab import ModesTab
+from .views.color_tab import ColorTab
 
-__all__ = ['DevicesTab', 'ModesTab', 'ColorTab']
+__all__ = ['run_gui', 'DevicesTab', 'ModesTab', 'ColorTab']
